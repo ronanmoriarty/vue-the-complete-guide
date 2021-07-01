a9aebb85c92cc2fdc161d1a57a912895ed4f8303
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-01 08:46:59 +0100

    Exercise 3

--- section-24/Exercise 3/app.js
+++ section-24/Exercise 3/app.js
@@ -1,6 +1,20 @@
 new Vue({
         el: '#exercise',
         data: {
-            value: 0
+            value: 0,
+            timeout: 5000
+        },
+        watch: {
+            result: function(value) {
+                const vm = this;
+                setTimeout(() => {
+                    this.value = 0;
+                }, this.timeout);
+            }
+        },
+        computed: {
+            result: function() {
+                return this.value < 37 ? 'Not there yet' : 'Done';
+            }
         }
     });
