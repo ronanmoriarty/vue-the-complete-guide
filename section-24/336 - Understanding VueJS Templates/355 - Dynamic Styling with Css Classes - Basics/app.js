eb0dd998e0bb827daf3cd644df272e4f1147fc85
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-01 09:14:48 +0100

    Determine dynamic classes using computed properties

--- section-24/336 - Understanding VueJS Templates/355 - Dynamic Styling with Css Classes - Basics/app.js
+++ section-24/336 - Understanding VueJS Templates/355 - Dynamic Styling with Css Classes - Basics/app.js
@@ -4,5 +4,13 @@ new Vue({
         attachRed: false,
         attachGreen: false,
         attachBlue: false
+    },
+    computed: {
+        divClasses: function() {
+            return {
+                red: this.attachRed,
+                blue: !this.attachRed
+            };
+        }
     }
 });
