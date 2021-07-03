16e917704cc5d939c9e2b08864f8e7cb7e64f6a5
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-03 09:17:57 +0100

    Looping through objects

--- section-25/367 - Rendering Lists with v-for/app.js
+++ section-25/367 - Rendering Lists with v-for/app.js
@@ -3,8 +3,8 @@ new Vue({
     data: {
         ingredients: ['meat', 'fruit', 'cookies'],
         persons: [
-            {name: 'Ronan', age: 21, colour: 'red'},
-            {name: 'Fi', age: 'unknown', colour: 'blue'},
+            {name: 'Ronan', age: 21, colour: 'blue'},
+            {name: 'Fi', age: 'unknown', colour: 'purple'},
         ]
     }
 });
