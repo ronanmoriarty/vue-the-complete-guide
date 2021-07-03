31e51de71b71cf7e5f356dbe3aea9b27706eff42
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-03 11:40:11 +0100

    Check if game over and make sure health max is 100

--- section-26/app.js
+++ section-26/app.js
@@ -14,20 +14,27 @@ new Vue({
             this.gameStarted = true;
             this.yourHealth = 100;
             this.monsterHealth = 100;
+            this.logs = [];
         },
         attack: function() {
             this.inflictDamageOnMonster(this.normalAttackLimit);
             this.inflictDamageOnPlayer1(this.normalAttackLimit);
+            this.checkIfGameOver();
         },
         specialAttack: function() {
             this.inflictDamageOnMonster(this.specialAttackLimit);
             this.inflictDamageOnPlayer1(this.specialAttackLimit);
+            this.checkIfGameOver();
         },
         heal: function() {
-            const healthBoost = Math.floor(Math.random() * this.healthBoostLimit);
+            let healthBoost = Math.floor(Math.random() * this.healthBoostLimit);
+            if(healthBoost + this.yourHealth > 100) {
+                healthBoost = 100 - this.yourHealth;
+            }
             this.yourHealth += healthBoost;
             this.updateLog(`Player gets health boost of ${healthBoost}`);
             this.inflictDamageOnPlayer1(this.normalAttackLimit);
+            this.checkIfGameOver();
         },
         inflictDamageOnMonster: function(limit) {
             const damage = this.getRandomDamage(limit);
@@ -44,6 +51,19 @@ new Vue({
                 this.yourHealth = 0;
             }
             this.updateLog(`Monster hits player. Inflicts damage of ${damage}`);
+        },
+        checkIfGameOver: function() {
+            if(this.yourHealth === 0 && this.monsterHealth === 0) {
+                this.updateLog('Draw');
+            } else if (this.yourHealth === 0) {
+                this.updateLog('Monster won');
+            } else if (this.monsterHealth === 0) {
+                this.updateLog('You won');
+            }
+
+            if(this.yourHealth === 0 || this.monsterHealth === 0) {
+                this.gameStarted = false;
+            }
         },
         updateLog: function(message) {
             this.logs.push(message);
