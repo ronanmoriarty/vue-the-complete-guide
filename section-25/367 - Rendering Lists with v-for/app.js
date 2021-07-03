5a377c1ee8be29e1acb82f26aebe0cbbefa17853
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2021-07-03 09:58:02 +0100

    Section 25 - Using Conditionals and Rendering Lists [VUE2] (#4)
    * Add project starter template
    
    * Conditional rendering with v-if
    
    * Use template to group elements without introducing extra wrapping element
    
    * Use v-show to hide while keeping it in the DOM
    
    * Demo v-for
    
    * Add index to v-for
    
    * Using v-for with template to repeat multiple elements
    
    * Looping through objects
    
    * Loop through numbers
    
    * vue proxy to watch for array elements changing, and binding unique key to resolve list re-ordering issues
    
    * Exercise for section 25
    
    Co-authored-by: Ronan <ronan.moriarty@justeattakeaway.com>

---
+++ section-25/367 - Rendering Lists with v-for/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        ingredients: ['meat', 'fruit', 'cookies'],
+        persons: [
+            {name: 'Ronan', age: 21, colour: 'blue'},
+            {name: 'Fi', age: 'unknown', colour: 'purple'},
+        ]
+    }
+});
