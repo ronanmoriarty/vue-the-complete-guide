0ee9e5f083a0b85e1823622061204d96d69c0ee0
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-03 10:47:48 +0100

    Set health bars width and text according to current scores

--- section-26/app.js
+++ section-26/app.js
@@ -1,14 +1,30 @@
 new Vue({
     el: '#app',
     data: {
-        gameStarted: false
+        gameStarted: false,
+        yourHealth: 100,
+        monsterHealth: 100
     },
     methods: {
         startGame: function() {
             this.gameStarted = true;
+            this.yourHealth = 100;
+            this.monsterHealth = 100;
         },
         giveUp: function() {
             this.gameStarted = false;
+        }
+    },
+    computed: {
+        yourHealthStyle: function() {
+            return {
+                width: `${this.yourHealth}%`
+            }
+        },
+        monsterHealthStyle: function() {
+            return {
+                width: `${this.monsterHealth}%`
+            }
         }
     }
 });
