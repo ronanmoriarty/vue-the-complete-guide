1c488fc9e85c9be07586dc5ab000d0351ca95442
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-03 11:02:38 +0100

    Add attack() and specialAttack()

--- section-26/app.js
+++ section-26/app.js
@@ -3,13 +3,32 @@ new Vue({
     data: {
         gameStarted: false,
         yourHealth: 100,
-        monsterHealth: 100
+        monsterHealth: 100,
+        normalAttackLimit: 20,
+        specialAttackLimit: 40
     },
     methods: {
         startGame: function() {
             this.gameStarted = true;
             this.yourHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function() {
+            this.inflictDamageOnMonster(this.normalAttackLimit);
+            this.inflictDamageOnPlayer1(this.normalAttackLimit);
+        },
+        specialAttack: function() {
+            this.inflictDamageOnMonster(this.specialAttackLimit);
+            this.inflictDamageOnPlayer1(this.specialAttackLimit);
+        },
+        inflictDamageOnMonster: function(limit) {
+            this.monsterHealth -= this.getRandomDamage(limit);
+        },
+        inflictDamageOnPlayer1: function(limit) {
+            this.yourHealth -= this.getRandomDamage(limit);
+        },
+        getRandomDamage: function(limit) {
+            return Math.floor(Math.random() * limit);
         },
         giveUp: function() {
             this.gameStarted = false;
