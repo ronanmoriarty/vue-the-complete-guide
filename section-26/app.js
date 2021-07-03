3af321c1dff7c599de6abe2cf735693903a5d688
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-03 12:34:14 +0100

    Use confirm and checkWin() to match video

--- section-26/app.js
+++ section-26/app.js
@@ -15,13 +15,19 @@ new Vue({
         },
         attack: function() {
             this.inflictDamageOnMonster(3, 20);
+            if(this.checkWin()) {
+                return;
+            }
             this.inflictDamageOnPlayer1(3, 20);
-            this.checkIfGameOver();
+            this.checkWin();
         },
         specialAttack: function() {
             this.inflictDamageOnMonster(10, 40);
+            if(this.checkWin()) {
+                return;
+            }
             this.inflictDamageOnPlayer1(10, 40);
-            this.checkIfGameOver();
+            this.checkWin();
         },
         heal: function() {
             let healthBoost = Math.floor(Math.random() * 20);
@@ -47,6 +53,24 @@ new Vue({
                 this.yourHealth = 0;
             }
             this.updateLog(`Monster hits player. Inflicts damage of ${damage}`);
+        },
+        checkWin: function() {
+            if(this.monsterHealth <= 0) {
+                if(confirm('You won! Start again?')) {
+                    this.startGame();
+                } else {
+                    this.gameStarted = false;
+                }
+                return true;
+            } else if(this.yourHealth <= 0) {
+                if(confirm('You lost! Start again?')) {
+                    this.startGame();
+                } else {
+                    this.gameStarted = false;
+                }
+                return true;
+            }
+            return false;
         },
         checkIfGameOver: function() {
             if(this.yourHealth === 0 && this.monsterHealth === 0) {
