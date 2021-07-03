53c58b806267113ac257699510e23fdaf670c483
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-03 08:59:46 +0100

    Demo v-for

---
+++ section-25/367 - Rendering Lists with v-for/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        ingredients: ['meat', 'fruit', 'cookies'],
+        persons: [
+            {name: 'Ronan', age: 21, colour: 'red'},
+            {name: 'Fi', age: 'unknown', colour: 'blue'},
+        ]
+    }
+});
