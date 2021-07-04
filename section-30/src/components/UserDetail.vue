876af5e12dc65469700d2cde8cad33ab2bfc85b0
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-04 18:04:42 +0100

    Emit event to pass data from child to parent

--- section-30/src/components/UserDetail.vue
+++ section-30/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
     <p>User Name: {{ switchName() }}</p>
+    <button @click="resetName">Reset Name</button>
   </div>
 </template>
 
@@ -11,12 +12,16 @@ export default {
     props: {
         myName: {
             type: String,
-            default: 'Sadhbh'
+            required: true
         }
     },
     methods: {
         switchName() {
             return this.myName.split("").reverse().join("");
+        },
+        resetName() {
+            this.myName = 'Ronan';
+            this.$emit('nameWasReset', this.myName);
         }
     }
 };
