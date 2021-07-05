cb0b33e6cf97350d9253756611eb9890fe35153e
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-05 08:44:19 +0100

    Intro to slots

--- section-31/src/components/Quote.vue
+++ section-31/src/components/Quote.vue
@@ -1,18 +1,10 @@
 <template>
     <div>
-        <p>{{ quote }}</p>
+        <slot></slot>
     </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            quote: {
-                type: String,
-                required: true
-            }
-        }
-    }
 </script>
 
 <style scoped>
