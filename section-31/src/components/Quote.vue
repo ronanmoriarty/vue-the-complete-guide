8bdc5317f09f7f0b8410996d88711538e0d1ab23
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-05 09:07:34 +0100

    Named slots

--- section-31/src/components/Quote.vue
+++ section-31/src/components/Quote.vue
@@ -1,22 +1,31 @@
 <template>
-    <div>
-        <slot></slot>
+  <div>
+    <div class="title">
+      <slot name="title"></slot>
     </div>
+    <hr />
+    <div>
+      <slot name="content"></slot>
+    </div>
+  </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style scoped>
-    div {
-        border: 1px solid black;
-        box-shadow: 1px 1px 2px black;
-        padding: 30px;
-        margin: 30px auto;
-        text-align: center;
-    }
+div {
+  border: 1px solid black;
+  box-shadow: 1px 1px 2px black;
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+}
 
-    h2 {
-        color: red;
-    }
+h2 {
+  color: red;
+}
+
+.title {
+  font-style: italic;
+}
 </style>
