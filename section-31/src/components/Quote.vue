91f3d62268096b7baa6edc1e57e84221cf13b15e
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-05 08:41:32 +0100

    Add Quote component - initially getting quote from prop

---
+++ section-31/src/components/Quote.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>{{ quote }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            quote: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid black;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+</style>
