55d9b484ee796c613d2b6a2e59ff5f42c0b90865
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2021-07-07 18:41:40 +0100

    Section 32 - Second Course Project - Wonderful Quotes [VUE2] (#12)
    * Add starter files
    
    * Add props to communicate from parent to child
    
    * Demo case sensitivity availabe and how props can be used like data
    
    * Add basic type validation for prop
    
    * Make prop required
    
    * Add a default for prop
    
    * Emit event to pass data from child to parent
    
    * Use callback as alternative to custom event
    
    * Demo communication between siblings
    
    * Demo using a separate Vue instance as an eventBus to avoid having to communicate through parent
    
    * Communicate to other sibling using custom method added to eventBus
    
    * Copy assignment-14 from section-29 to assignment-15
    
    * Assignment 15
    
    * Change to show status in server details
    
    * Add starter files
    
    * Add Quotes and Quote
    
    * Add New
    
    * Add Progress
    
    * Add remove() on clicking quote
    
    * Fix styling
    
    * Rename components and use bootstrap styling (in line with tutorial)
    
    * Add info box
    
    * Delete quotes at App component level
    
    * Change to follow along with tutorial
    
    Co-authored-by: Ronan <ronan.moriarty@justeattakeaway.com>

---
+++ section-32/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <h3>Quotes Added</h3>
+      <div class="progress">
+        <div
+          class="progress-bar"
+          role="progressbar"
+          aria-valuemin="0"
+          aria-valuemax="100"
+          :style="progressStyle"
+        >
+          <span>{{ quoteCount }} / {{ maxQuotes }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    quoteCount: {
+      type: Number,
+      required: true
+    },
+    maxQuotes: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    progressStyle() {
+      const progress = 100 * this.quoteCount / this.maxQuotes;
+      return {
+        "aria-valuenow": progress,
+        width: `${progress}%`,
+        transition: "width 0.4s ease-in-out",
+      };
+    },
+  },
+};
+</script>
---
+++ section-32/src/components/QuoteGrid.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(quote, index) in quotes" :key="quote" @click.native="remove(index)">
+      <span>{{ quote }}</span>
+    </app-quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+
+export default {
+  props: {
+    quotes: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    appQuote: Quote,
+  },
+  methods: {
+    remove(index) {
+        this.$emit('quoteDeleted', index);
+    }
+  }
+};
+</script>
