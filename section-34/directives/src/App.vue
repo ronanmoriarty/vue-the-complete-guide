f08f408574bf7daf3ea6367b149978c2decd7dfe
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-08 09:26:36 +0100

    Create a simple directive

--- section-34/directives/src/App.vue
+++ section-34/directives/src/App.vue
@@ -2,8 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
-
+                <h1>Built-in Directives</h1>
+                <p v-text="'some text'"></p>
+                <div v-html="'<strong>Some text in bold</strong>'"></div>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-highlight>Highlight</p>
             </div>
         </div>
     </div>
