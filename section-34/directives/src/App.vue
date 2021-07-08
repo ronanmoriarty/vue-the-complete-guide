3ef707974824f7a09ca235d81074a8dbe0767759
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-08 09:28:54 +0100

    Pass value to directive

--- section-34/directives/src/App.vue
+++ section-34/directives/src/App.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-highlight>Highlight</p>
+                <p v-highlight="'red'">Highlight</p>
             </div>
         </div>
     </div>
