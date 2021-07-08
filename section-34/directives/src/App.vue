2d13c48195b6f685e06380f23f5a10fa2356b5b2
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-08 09:57:54 +0100

    Using local directives with multiple modifiers

--- section-34/directives/src/App.vue
+++ section-34/directives/src/App.vue
@@ -1,26 +1,65 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Built-in Directives</h1>
-                <p v-text="'some text'"></p>
-                <div v-html="'<strong>Some text in bold</strong>'"></div>
-            </div>
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Custom Directives</h1>
-                <p v-highlight:background="'red'">Change background colour (by setting explicit background directive arg)</p>
-                <p v-highlight:background.delayed="'red'">Delay changing background colour (using modifiers)</p>
-                <p v-highlight="'red'">Change text colour (default)</p>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built-in Directives</h1>
+        <p v-text="'some text'"></p>
+        <div v-html="'<strong>Some text in bold</strong>'"></div>
+      </div>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight:background="'red'">
+          Change background colour (by setting explicit background directive
+          arg)
+        </p>
+        <p v-highlight:background.delayed="'red'">
+          Delay changing background colour (using modifiers)
+        </p>
+        <p v-highlight="'red'">Change text colour (default)</p>
+        <p v-local-highlight:background.delayed.blink="'red'">
+          Local directive
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColour = binding.value;
+          let secondColour = "blue";
+          let currentColour = mainColour;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColour === mainColour ? currentColour = secondColour : currentColour = mainColour;
+              if (binding.arg === "background") {
+                el.style.backgroundColor = currentColour;
+              } else {
+                el.style.color = currentColour;
+              }
+            }, 1000);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg === "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+      },
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
