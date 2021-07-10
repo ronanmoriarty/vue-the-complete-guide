b0126ef76481148dbe5c25f19b3d9c3b81a73ac0
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-10 12:59:36 +0100

    How to specify enter/leave active classes explicitly

--- section-36/animations/src/App.vue
+++ section-36/animations/src/App.vue
@@ -4,15 +4,22 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br>
                 <button class="btn btn-primary" @click="show = !show">Show Alert</button>
                 <br><br>
-                <transition name="fade">
+                <transition :name="alertAnimation">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
                 <transition name="slide" type="animation">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
-                <transition name="fade" appear>
+                <transition
+                    enter-active-class="animated bounce"
+                    leave-active-class="animated shake">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
             </div>
@@ -24,7 +31,8 @@
     export default {
         data() {
             return {
-                show: true
+                show: true,
+                alertAnimation: 'fade'
             }
         }
     }
