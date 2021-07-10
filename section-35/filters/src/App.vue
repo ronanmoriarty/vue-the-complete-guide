25c28deae104a6972cb531c85276620167f5a409
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-10 10:20:36 +0100

    Demonstrate mixin scope (ie. not shared between components)

--- section-35/filters/src/App.vue
+++ section-35/filters/src/App.vue
@@ -4,6 +4,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
         <p>{{ text | toUppercase | to-lowercase }}</p>
+        <button @click="fruits.push('Berries')">Add New Item</button>
         <input v-model="filterText" />
         <ul>
           <li v-for="fruit in filteredFruits">{{ fruit }}</li>
