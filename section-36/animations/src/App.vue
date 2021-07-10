0f4d7b001e05a97c6b3805ed9fc55d139fc88179
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-10 11:16:15 +0100

    Use css transition with vue transition element to fade in and out

--- section-36/animations/src/App.vue
+++ section-36/animations/src/App.vue
@@ -3,7 +3,12 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-
+                <hr>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition name="fade">
+                    <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
             </div>
         </div>
     </div>
@@ -13,12 +18,26 @@
     export default {
         data() {
             return {
-
+                show: false
             }
         }
     }
 </script>
 
 <style>
+    .fade-enter {
+        opacity: 0;
+    }
 
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+
+    .fade-leave {
+    }
+
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
 </style>
