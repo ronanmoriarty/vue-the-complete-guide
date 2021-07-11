ffe4298d93024be114ccb4f577583ac5168f3bd2
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-11 12:40:34 +0100

    Move base url to global options

--- section-37/vue-resource/src/App.vue
+++ section-37/vue-resource/src/App.vue
@@ -36,7 +36,7 @@
         },
         methods: {
             submit() {
-                this.$http.post('https://vuejs-http-180c1-default-rtdb.europe-west1.firebasedatabase.app/data.json', this.user)
+                this.$http.post('', this.user)
                     .then(response => {
                         console.log(response);
                     }, error => {
@@ -44,7 +44,7 @@
                     });
             },
             fetchData() {
-                this.$http.get('https://vuejs-http-180c1-default-rtdb.europe-west1.firebasedatabase.app/data.json')
+                this.$http.get('')
                     .then(response => {
                         return response.json();
                     }).then(data => {
--- section-37/vue-resource/src/main.js
+++ section-37/vue-resource/src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 
 Vue.use(VueResource);
 
+Vue.http.options.root = 'https://vuejs-http-180c1-default-rtdb.europe-west1.firebasedatabase.app/data.json';
+
 new Vue({
   el: '#app',
   render: h => h(App)
