f6d31f8ac86ef3cdca7fede167d8c5ae587b0407
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-11 17:27:29 +0100

    Set up child routes (aka nested routes)

--- section-38/routing/src/components/user/User.vue
+++ section-38/routing/src/components/user/User.vue
@@ -2,23 +2,14 @@
     <div>
     <h1>The User Page</h1>
     <hr>
-    <p>Loaded id: {{ id }}</p>
     <button @click="navigateToHome">Go To Home</button>
+    <hr>
+    <router-view></router-view>
 </div>
 </template>
 
 <script>
 export default {
-    data () {
-        return {
-            id: this.$route.params.id
-        }
-    },
-    watch: {
-        '$route'(to, from) {
-            this.id = to.params.id;
-        }
-    },
     methods: {
         navigateToHome() {
             this.$router.push('/');
