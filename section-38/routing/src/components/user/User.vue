e83c5076545d1ffcf971668d637a9847926271be
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-11 16:27:20 +0100

    Navigate from code

--- section-38/routing/src/components/user/User.vue
+++ section-38/routing/src/components/user/User.vue
@@ -1,4 +1,19 @@
 <template>
+    <div>
     <h1>The User Page</h1>
-
+    <hr>
+    <button @click="navigateToHome">Go To Home</button>
+</div>
 </template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome() {
+            this.$router.push('/');
+            // Alternatively, we can pass an object...
+            // this.$router.push({ path: '/'});
+        }
+    }
+}
+</script>
