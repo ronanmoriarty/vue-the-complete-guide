c6cdb398451728c29b4f1654da22801406e43d62
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2021-07-11 20:58:17 +0100

    Section 38 - Routing in a VueJS Application [VUE2] (#18)
    * Add starter files
    
    * Install vue-router
    
    * Add routing
    
    * Change to history mode so we do not need /#/ in url
    
    * Use router-links to load different routes without going to server
    
    * Style active links with tag, active-class and exact attributes
    
    * Navigate from code
    
    * Route parameters
    
    * Watch $route to update when route params change
    
    * Set up child routes (aka nested routes)
    
    * Add router-links to nested routes and get route params
    
    * Construct edit link
    
    * Use named routes to avoid having to construct url
    
    * Use query parameters
    
    * Use named router-view
    
    * Redirecting
    
    * Add catch-all which does redirect to home
    
    * Add animation to route changes
    
    * Pass hash data
    
    * Add scroll behaviour - savedPosition if it exists, otherwise check for hash, otherwise default x, y
    
    * beforeEnter guards
    
    * beforeRouteLeave guard
    
    * Lazy load routing
    
    Co-authored-by: Ronan <ronan.moriarty@justeattakeaway.com>

---
+++ section-38/routing/src/components/user/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <h1>The User Page</h1>
+    <hr>
+    <button class="btn btn-primary" @click="navigateToHome">Go To Home</button>
+    <hr>
+    <router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome() {
+            // this.$router.push('/');
+            // Alternatively, we can pass an object...
+            this.$router.push({ name: 'home'});
+        }
+    }
+}
+</script>
