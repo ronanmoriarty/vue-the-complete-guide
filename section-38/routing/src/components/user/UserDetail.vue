8102d05b9dd6c84e91b97f5e32bd81333a1c7491
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-11 17:46:52 +0100

    Construct edit link

--- section-38/routing/src/components/user/UserDetail.vue
+++ section-38/routing/src/components/user/UserDetail.vue
@@ -2,5 +2,10 @@
   <div>
     <h3>Some User Details</h3>
     <p>User loaded has ID: {{ $route.params.id }}</p>
+    <router-link
+      :to="'/user/' + $route.params.id + '/edit'"
+      class="btn btn-primary"
+      >Edit User</router-link
+    >
   </div>
 </template>
