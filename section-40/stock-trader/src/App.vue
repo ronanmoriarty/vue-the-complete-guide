99c99e1d98bf5c157aade4abbcdc5716a5ee90f0
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-13 09:12:36 +0100

    Style header using bootstrap

--- section-40/stock-trader/src/App.vue
+++ section-40/stock-trader/src/App.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-md-12">
         <router-view></router-view>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Header from './components/Header.vue';
-    export default {
-        components: {
-            appHeader: Header
-        }
-    }
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    appHeader: Header,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
