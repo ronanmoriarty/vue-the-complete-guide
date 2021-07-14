3597d8e1557803e3b7b69923b364d40b5d73a56b
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-14 20:57:42 +0100

    Add animation between route changes

--- section-40/stock-trader/src/App.vue
+++ section-40/stock-trader/src/App.vue
@@ -3,7 +3,9 @@
     <app-header></app-header>
     <div class="row">
       <div class="col-md-12">
-        <router-view></router-view>
+        <transition mode="out-in" name="fade">
+            <router-view></router-view>
+        </transition>
       </div>
     </div>
   </div>
@@ -24,5 +26,40 @@ export default {
 <style>
     body {
         padding: 30px;
+    }
+
+    .fade-enter {
+
+    }
+
+    .fade-enter-active {
+        animation: fade-in 0.25s ease-out forwards;
+    }
+
+    .fade-leave {
+    }
+
+    .fade-leave-active {
+        animation: fade-out 0.25s ease-out forwards;
+    }
+
+    @keyframes fade-out {
+        from {
+            opacity: 1;
+        }
+
+        to {
+            opacity: 0;
+        }
+    }
+
+    @keyframes fade-in {
+        from {
+            opacity: 0;
+        }
+
+        to {
+            opacity: 1;
+        }
     }
 </style>
