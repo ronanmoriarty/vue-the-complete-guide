ef1fff78b84c41a7493900c795a622df04b2c13f
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-14 09:19:22 +0100

    Change portfolio module to match tutorial

--- section-40/stock-trader/src/store/modules/portfolio.js
+++ section-40/stock-trader/src/store/modules/portfolio.js
@@ -1,30 +1,54 @@
 const state = {
-    orders: [],
-    balance: 10000
-};
-
-const getters = {
-    orders: state => {
-        return state.orders;
-    },
-    balance: state => {
-        return state.balance;
-    }
+    stocks: [],
+    funds: 10000
 };
 
 const mutations = {
-    purchase: (state, order) => {
-        if(order.stockPrice * order.quantity < state.balance) {
-            state.orders.push(order);
+    'BUY_STOCK'(state, { stockId, quantity, stockPrice }) {
+        let stock = state.stocks.find(stock => stock.id === stockId);
+        if(stock) {
+            stock.quantity += quantity;
         } else {
-            // what should we do if not enough funds? How should we communicate this?
+            state.stocks.push({
+                id: stockId,
+                quantity
+            });
         }
+
+        state.funds -= stockPrice * quantity;
+    },
+    'SELL_STOCK'(state, { stockId, quantity, stockPrice }) {
+        let stock = state.stocks.find(stock => stock.id === stockId);
+        if(stock.quantity > quantity) {
+            stock.quantity -= quantity;
+        } else {
+            state.stocks.splice(state.stocks.indexOf(stock));
+        }
+
+        state.funds += stockPrice * Math.min(quantity, stock.quantity);
     }
 };
 
 const actions = {
-    purchase: ({ commit }, order) => {
+    sellStock({ commit }, order) {
         commit('purchase', order);
+    }
+};
+
+const getters = {
+    stockPortfolio: (state, getters) => {
+        return state.stocks.map(stock => {
+            const record = getters.stocks.find(element => element.id === stock.id);
+            return {
+                id: stock.id,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            };
+        });
+    },
+    funds: state => {
+        return state.funds;
     }
 };
 
