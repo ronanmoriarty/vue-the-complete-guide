1374246d5c8fc43be3dde1042ea5bd56ec2b92a1
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-14 08:37:28 +0100

    Initial draft for portfolio vuex module

--- section-40/stock-trader/src/store/modules/portfolio.js
+++ section-40/stock-trader/src/store/modules/portfolio.js
@@ -1,3 +1,36 @@
-export default {
+const state = {
+    orders: [],
+    balance: 10000
+};
 
+const getters = {
+    orders: state => {
+        return state.orders;
+    },
+    balance: state => {
+        return state.balance;
+    }
+};
+
+const mutations = {
+    purchase: (state, order) => {
+        if(order.stockPrice * order.quantity < state.balance) {
+            state.orders.push(order);
+        } else {
+            // what should we do if not enough funds? How should we communicate this?
+        }
+    }
+};
+
+const actions = {
+    purchase: ({ commit }, order) => {
+        commit('purchase', order);
+    }
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions,
 }
