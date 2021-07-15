7c9cf4c95aed5743fe82f5331508a9d08745bb84
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-15 18:18:12 +0100

    Update firebase url

--- section-43/authentication/src/main.js
+++ section-43/authentication/src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import router from './router'
 import store from './store'
 
-axios.defaults.baseURL = 'https://vue-update.firebaseio.com'
+axios.defaults.baseURL = 'https://vue-axios-19f76-default-rtdb.europe-west1.firebasedatabase.app'
 axios.defaults.headers.common['Authorization'] = 'fasfdsa'
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
