1373fbfc6b88e9055f68570f40bc9e9e80e81aa7
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-15 13:24:27 +0100

    Map email address from GET

--- section-42/axios/src/components/dashboard/dashboard.vue
+++ section-42/axios/src/components/dashboard/dashboard.vue
@@ -2,8 +2,36 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your email address: {{ email }}</p>
   </div>
 </template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        email: ''
+      }
+    },
+    created() {
+      axios.get('https://vue-axios-19f76-default-rtdb.europe-west1.firebasedatabase.app/users.json')
+        .then(response => {
+          const data = response.data;
+          const users = [];
+          for(let key in data) {
+            const user = data[key];
+            user.id = key;
+            users.push(user);
+          }
+          console.log(response);
+          console.log(users);
+          this.email = users[0].email;
+        }).catch(error => console.log(error));
+    }
+  }
+</script>
 
 <style scoped>
   h1, p {
