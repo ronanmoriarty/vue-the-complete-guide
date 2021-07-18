71d84d489d386ada77e31e4e8be7dc059e6c378c
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-18 15:31:29 +0100

    Add caching around loading coaches

--- section-16/find-a-coach/src/store/modules/coaches/actions.js
+++ section-16/find-a-coach/src/store/modules/coaches/actions.js
@@ -24,7 +24,12 @@ export default {
             id: userId
         });
     },
-    async loadCoaches(context) {
+    async loadCoaches(context, payload) {
+        if(!payload.forceRefresh && !context.getters.shouldUpdate) {
+            return;
+        }
+
+        console.log('Loading coaches...');
         const response = await fetch('https://vue-coach-finder-4c88d-default-rtdb.europe-west1.firebasedatabase.app/coaches.json');
         const responseData = await response.json();
         if(!response.ok) {
@@ -47,5 +52,6 @@ export default {
         }
 
         context.commit('setCoaches', coaches);
+        context.commit('setFetchTimestamp');
     }
 };
