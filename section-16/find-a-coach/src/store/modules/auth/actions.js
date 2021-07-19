1c06b59beb7caa6cbaf701398a8c7685818172fe
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-19 08:59:59 +0100

    Add auto-login

--- section-16/find-a-coach/src/store/modules/auth/actions.js
+++ section-16/find-a-coach/src/store/modules/auth/actions.js
@@ -1,7 +1,25 @@
 export default {
   async login(context, payload) {
+    return context.dispatch('auth', {
+        ...payload,
+        mode: 'login'
+    });
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+        ...payload,
+        mode: 'signup'
+    });
+  },
+  async auth(context, payload) {
+    console.log('auth....');
+    const mode = payload.mode;
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBdmJMq9FAq74cAhcGz_AzFvLzg3knasy0';
+    if(mode === 'signup') {
+        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBdmJMq9FAq74cAhcGz_AzFvLzg3knasy0';
+    }
     const response = await fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBdmJMq9FAq74cAhcGz_AzFvLzg3knasy0',
+        url,
         {
           method: 'POST',
           body: JSON.stringify({
@@ -15,51 +33,36 @@ export default {
       const responseData = await response.json();
 
       if (!response.ok) {
-        console.log(responseData);
         const error = new Error(responseData.message || 'Failed to login');
         throw error;
       }
 
-      console.log(responseData);
+      localStorage.setItem('token', responseData.idToken);
+      localStorage.setItem('userId', responseData.localId);
+    //   localStorage.setItem('expires', responseData.expiresIn); // need to convert to absolute date and time - at the moment it's just "3600", i.e. number of seconds to expiry
+
       context.commit('setUser', {
-          token: responseData.idToken,
-          userId: responseData.localId,
-          tokenExpiration: responseData.expiresIn // comes back from firebase as string representing number of seconds to expire - will transform this later.
-      });
-  },
-  async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBdmJMq9FAq74cAhcGz_AzFvLzg3knasy0',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(responseData.message || 'Failed to signup');
-      throw error;
-    }
-
-    console.log(responseData);
-    context.commit('setUser', {
         token: responseData.idToken,
         userId: responseData.localId,
         tokenExpiration: responseData.expiresIn // comes back from firebase as string representing number of seconds to expire - will transform this later.
-    });
+      });
+  },
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    if(token && userId) {
+        context.commit('setUser', {
+            token,
+            userId,
+            tokenExpiration: null
+        });
+    }
   },
   logout(context) {
     context.commit('setUser', {
-        token: null,
-        userId: null,
-        tokenExpiration: null
+      token: null,
+      userId: null,
+      tokenExpiration: null
     });
   }
 };
