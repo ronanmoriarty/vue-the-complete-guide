e493a71d94f1789907661d7a399e00b7b68addff
Ronan <ronan.moriarty@justeattakeaway.com>
2021-07-25 12:58:22 +0100

    Move AddGoals and CoarseGoals to composition api

--- section-22/migration/composition-api/src/components/CourseGoals.vue
+++ section-22/migration/composition-api/src/components/CourseGoals.vue
@@ -8,32 +8,30 @@
 <script>
 import GoalsList from "./GoalsList.vue";
 import AddGoal from "./AddGoal.vue";
+import { ref, computed } from 'vue';
 
 export default {
   components: {
     GoalsList,
     AddGoal,
   },
-  data() {
-    return {
-      goals: [],
-    };
-  },
-  computed: {
-    filteredGoals() {
-      return this.goals.filter(
-        (goal) => !goal.text.includes("Angular") && !goal.text.includes("React")
-      );
-    },
-  },
-  methods: {
-    addGoal(text) {
+  setup() {
+    const goals = ref([]);
+    const filteredGoals = computed(() => {
+      return goals.value.filter(goal => !goal.text.includes("Angular") && !goal.text.includes("React"));
+    });
+    function addGoal(text) {
       const newGoal = {
         id: new Date().toISOString(),
         text: text,
       };
-      this.goals.push(newGoal);
-    },
-  },
+      goals.value.push(newGoal);
+    }
+
+    return {
+      filteredGoals,
+      addGoal
+    }
+  }
 };
 </script>
