a7a6a49dde8b024f1dff7a595063e8e2e645f9a3
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2021-07-25 09:43:47 +0100

    Section 20 - Reusing Functionality: Mixins & Custom Composition Functions (#26)
    * Add starter files
    
    * Remove duplication with mixin
    
    * Component wins when there is a component-mixin clash
    
    * Add global mixin
    
    * Add starter files for composition api reuse
    
    * Create composable function to reuse setups between components
    
    * Passing args to hook
    
    * Add starter files for reuse-search-hook example
    
    * Extract search.js composable
    
    * Defer to search.js to do updates on its own data
    
    * Extract sort composable
    
    Co-authored-by: Ronan <ronan.moriarty@justeattakeaway.com>

---
+++ section-20/reuse-search-hook-example/src/components/users/UserList.vue
@@ -0,0 +1,56 @@
+<template>
+  <base-container>
+    <h2>Active Users</h2>
+    <base-search @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <div>
+      <button @click="sort('asc')" :class="{selected: sorting === 'asc'}">Sort Ascending</button>
+      <button @click="sort('desc')" :class="{selected: sorting === 'desc'}">Sort Descending</button>
+    </div>
+    <ul>
+      <user-item
+        v-for="user in displayedUsers"
+        :key="user.id"
+        :user-name="user.fullName"
+        :id="user.id"
+        @list-projects="$emit('list-projects', $event)"
+      ></user-item>
+    </ul>
+  </base-container>
+</template>
+
+<script>
+import { toRefs } from 'vue';
+import useSearch from '../../hooks/search';
+import useSort from '../../hooks/sort';
+
+import UserItem from './UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  props: ['users'],
+  emits: ['list-projects'],
+  setup(props) {
+    const { users } = toRefs(props);
+    const { availableItems, updateSearch, enteredSearchTerm } = useSearch(users, 'fullName');
+    const { sorting, displayedUsers, sort } = useSort(availableItems, 'fullName');
+
+    return {
+      enteredSearchTerm,
+      updateSearch,
+      displayedUsers,
+      sorting,
+      sort
+    };
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
